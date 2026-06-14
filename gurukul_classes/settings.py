14d5642c20d712f09f613f1a0a67855546896a9b
Priyanshu847203 <kumarpriyanshu8809@gmail.com>
2026-06-14 10:47:01 +0530

    Fixed static files for Render

--- gurukul_classes/settings.py
+++ gurukul_classes/settings.py
@@ -119,6 +119,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+import os
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
